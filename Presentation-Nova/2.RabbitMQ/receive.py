b551f5feda6adbaddbca8024c2616187dc05368d
khanhnt9 <ntkhanh99.vn@gmail.com>
2021-05-05 18:20:54 +0700

    done RabbitMQ RPC

---
+++ Presentation-Nova/2.RabbitMQ/receive.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+def callback(ch, method, properties, body):
+    print("[x] Received %r" % body)
+
+channel.basic_consume(queue='hello', auto_ack=True, on_message_callback=callback)
+
+print(' [*] Waiting for messages')
+channel.start_consuming()

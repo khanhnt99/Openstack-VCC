a322c745d35a826673ef80e5009c3b5b01081473
khanhnt9 <ntkhanh99.vn@gmail.com>
2021-05-06 17:39:13 +0700

    done cell
    Signed-off-by: khanhnt9 <ntkhanh99.vn@gmail.com>

---
+++ Presentation-Nova/2.RabbitMQ/rpc_server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='rpc_queue')
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+def on_request(ch, method, props, body):
+    n = int(body)
+
+    print("[.] fib(%s)", %n)
+    response = fib(n)
+
+    
